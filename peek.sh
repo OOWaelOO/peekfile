20da538828cf1eb3c4875354f34033bcd080e77e
Wael Badr <waelbadr01@gmail.com>
2024-10-31 12:28:30 +0100

    Add warning if displaying only the first and last X lines of the fileand not full file

--- peek.sh
+++ peek.sh
@@ -4,6 +4,13 @@ else
   lines=$2 
 fi
 
+total_lines=$(cat $1 | wc -l)
+
+if [[ $total_lines -le $((2 * $lines)) ]]; then
+  cat "$1"
+else
+  echo "Warning: File has more than $((2 * $lines)) lines. Showing first and last $lines lines."
 head -n $lines $1
 echo "..."
 tail -n $lines $1 
+fi
